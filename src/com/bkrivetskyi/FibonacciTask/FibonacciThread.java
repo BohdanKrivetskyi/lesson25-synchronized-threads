b25189bacccb855d0f8e46d131114f6ac6d06cdb
bkrivetskyi <krivettskiy@gmail.com>
2021-01-20 11:07:43 +0200

    Fibonacci method Runnable for implemented.

---
+++ src/com/bkrivetskyi/FibonacciTask/FibonacciThread.java
@@ -0,0 +1,31 @@
+package com.bkrivetskyi.FibonacciTask;
+
+import java.math.BigInteger;
+
+public class FibonacciThread implements Runnable {
+
+    private int number;
+
+    public FibonacciThread(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.println("Calculation Fibonacci start" + number);
+            if (number <= 1) System.out.println(BigInteger.valueOf(number));
+
+            BigInteger previous = BigInteger.ZERO, next = BigInteger.ONE, sum;
+
+            for (int i = 2; i <= number; i++) {
+                sum = previous;
+                previous = next;
+                next = sum.add(previous);
+                System.out.println("Sum= " + sum + " + " + previous);
+            }
+
+            System.out.println("Result: " + next);
+        }
+    }
+}
