e3816ed5e968839da89cf6b222086925a5dac4b8
bkrivetskyi <krivettskiy@gmail.com>
2021-01-20 10:21:34 +0200

    Point creared.

--- src/com/bkrivetskyi/taskPoint/Point.java
+++ src/com/bkrivetskyi/taskPoint/Point.java
@@ -1,4 +1,19 @@
 package com.bkrivetskyi.taskPoint;
 
 public class Point {
+    private int x;
+    private int y;
+
+    public void move(int dx, int dy) {
+        x += dx;
+        y += dy;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
